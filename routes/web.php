11edcf312e04d386734b21be305c83614d26161e
itsmitchy <malforque717@gmail.com>
2017-12-19 15:57:04 +0800

    added routes for post

--- routes/web.php
+++ routes/web.php
@@ -20,18 +20,20 @@ Route::get('logout', 'LoginController@logout')->name('logout');
 Route::get('register', 'RegisterController@registerForm')->name('register');
 Route::post('register', 'RegisterController@register');
 Route::group(['middleware' => ['auth']], function(){
+
+    //admin
     Route::get('admin/dashboard', ['as' => 'admin', function(){
         return view('admin/admin');
     }]);
-   // Route::get('bidder', ['as' => 'bidderAdmin', 'uses' => 'BidderController@bidderView']);
     Route::get('list/bidder', ['as' => 'bidderAdmin', 'uses' => 'BidderController@getRole']);
-    //Route::get('developer', 'BidderController@getRole')->name);
-   // Route::get('admin.seeker', ['as' => 'adminSeeker', 'uses' => 'SeekerController@seekerData']);
     Route::get('list/seeker', ['as' => 'adminSeeker', 'uses' => 'SeekerController@getRole']);
     Route::get('calendar', ['as' => 'calendarAdmin', function(){
         return view('admin/calendar');
-    }]); 
-    //Route::get('seeker',['as' => 'seeker', 'uses' => 'ProjectController@seekerView']);
+    }]);
+    Route::get('list/post', ['as' => 'postAdmin', function(){
+        return view('admin/post');
+    }]);
+    //seeker
     Route::get('seeker', ['as' => 'seeker', 'uses' => 'ProjectController@getProjects']);
     Route::post('seeker', ['uses' => 'ProjectController@create']);
 });
