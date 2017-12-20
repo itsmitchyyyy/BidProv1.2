76d8fd32fc6068b558dc3441a6d1440223f88bbe
Mitch Alforque <malforque717@gmail.com>
2017-12-20 17:59:02 +0800

    resets update

---
+++ app/Http/Controllers/ResetController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Auth;
+class ResetController extends Controller
+{
+    public function sendEmail(Request $request){
+        $credentials = ['email' => $request->email];
+        $response = Password::sendResetLink($credentials, function(Message $message){
+            $message->subject($this->getEmailSubject());
+        }); 
+    
+
+       switch ($response) {
+            case Password::RESET_LINK_SENT:
+                return redirect()->back()->with('status', trans($response));
+             break;
+            case Password::INVALID_USER:
+                return redirect()->back()->withErrors(['email' => trans($response)]);
+        }
+    }
+
+    public function showReset(){
+        return view('email');
+    }
+
+    protected function getEmailSubject()
+    {
+        return isset($this->subject) ? $this->subject : 'Your password reset link';
+    }
+    
+    public function getReset($token = null)
+    {
+        if(is_null($token)){
+            throw new NotFoundHttpException;
+        }
+        return view('reset')->with('token', $token);
+    }
+
+    public function postReset(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        $credentials = $request->only('email', 'password', 'password_confirmation', 'token');
+        $response = Password::reset($credentials, function($user,$password){
+            $this->resetPassword($user,$password);
+        });
+
+        switch ($response) {
+            case Password::PASSWORD_RESET:
+                return redirect()->route('login');
+                break;
+            
+            default:
+                return redirect()->back()
+                    ->withInput()
+                    ->withErrors(['email' => trans($response)]);
+                break;
+        }
+    }
+
+    protected function resetPassword($user, $password)
+    {
+        $user->password = bcrypt($password);
+        $user->save();
+        Auth::login($user);
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -40,7 +40,7 @@
                                 <input id="checkbox-signup" type="checkbox">
                                 <label for="checkbox-signup"> Remember me </label>
                             </div>
-                            <a href="javascript:void(0)" id="to-recover" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> Forgot pwd?</a> </div>
+                            <a href="{{ route('password.email') }}" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> Forgot pwd?</a> </div>
                     </div>
                     <div class="form-group text-center m-t-20">
                         <div class="col-xs-12">
@@ -62,23 +62,7 @@
                         </div>
                     </div>
                 </form>
-                <form class="form-horizontal" id="recoverform" action="http://eliteadmin.themedesigner.in/demos/eliteadmin-material/index.html">
-                    <div class="form-group ">
-                        <div class="col-xs-12">
-                            <h3>Recover Password</h3>
-                            <p class="text-muted">Enter your Email and instructions will be sent to you! </p>
-                        </div>
-                    </div>
-                    <div class="form-group ">
-                        <div class="col-xs-12">
-                            <input class="form-control" type="text" required="" placeholder="Email">
-                        </div>
-                    </div>
-                    <div class="form-group text-center m-t-20">
-                        <div class="col-xs-12">
-                            <button class="btn btn-primary btn-lg btn-block text-uppercase waves-effect waves-light" type="submit" style="background-color:#ee4b28;border:2px solid #ee4b28">Reset</button>
-                        </div>
-                    </div>
+                
                 </form>
             </div>
         </div>
