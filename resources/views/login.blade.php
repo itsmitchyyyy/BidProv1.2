117c199ea8c060b2229d7b059c0e7075f25907db
itsmitchy <malforque717@gmail.com>
2017-12-21 16:06:17 +0800

    new form for login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,7 +10,7 @@
     <section id="wrapper" class="login-register">
         <div class="login-box">
             <div class="white-box">
-                <form class="form-horizontal form-material" method="POST" id="loginform" action="{{ route('login') }}">
+                <form class="floating-labels" method="POST" id="loginform" action="{{ route('login') }}">
                     {{ csrf_field() }}    
                 <h3 class="box-title m-b-20">Sign In</h3>
                     <div class="text-center form-group{{ $errors->has('error') ? ' has-error' : ''}}">
@@ -18,7 +18,17 @@
                         <p class="help-block">{{ $errors->first('error') }}</p>
                         @endif
                     </div>
-                    <div class="form-group{{ $errors->has('username') ? ' has-error' : ''}}">
+                    <div class="form-group m-b-40 m-t-15">
+                        <input type="text" name="username" id="username" class="form-control" style="border-bottom-color:#c0c0c0" required>
+                        <span class="highlight"></span><span class="bar"></span>
+                        <label for="username">Username</label>
+                    </div>
+                    <div class="form-group m-b-10 m-t-10">
+                        <input type="password" name="password" id="password" class="form-control" style="border-bottom-color:#c0c0c0" required>
+                        <span class="highlight"></span><span class="bar"></span>
+                        <label for="password">Password</label>
+                    </div>
+                   <!-- <div class="form-group{{ $errors->has('username') ? ' has-error' : ''}}">
                         <div class="col-xs-12">
                             <input class="form-control" type="text" name="username" required="" placeholder="Username">
                         </div>
@@ -33,20 +43,24 @@
                         @if($errors->has('password'))
                         <p class="help-block">{{ $errors->first('password') }}</p>
                         @endif
-                    </div>
-                    <div class="form-group">
-                        <div class="col-md-12">
+                    </div>-->
+                    
+                    <div class="form-group m-b-10 m-t-15">
+                        <!--<div class="col-md-12">
                             <div class="checkbox checkbox-primary pull-left p-t-0" style>
                                 <input id="checkbox-signup" type="checkbox">
                                 <label for="checkbox-signup"> Remember me </label>
                             </div>
+                            -->
                             <a href="{{ route('password.email') }}" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> Forgot pwd?</a> </div>
                     </div>
-                    <div class="form-group text-center m-t-20">
+                    
+                    <div class="form-group text-center m-t-10">
                         <div class="col-xs-12">
                             <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" style="background-color:#ee4b28;border:2px solid #ee4b28" type="submit">Log In</button>
                         </div>
                     </div>
+                    </form>
                     <!--
                     <div class="row">
                         <div class="col-xs-12 col-sm-12 col-md-12 m-t-10 text-center">
@@ -61,9 +75,9 @@
                             <p>Don't have an account? <a href="{{ route('register') }}" class="text-primary m-l-5"><b>Sign Up</b></a></p>
                         </div>
                     </div>
-                </form>
                 
-                </form>
+                
+                
             </div>
         </div>
     </section>
