3bbfb87c413348c0efa202cb81f8797ec8a4b251
Mitch Alforque <malforque717@gmail.com>
2017-12-21 19:25:03 +0800

    new login style

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,21 +13,7 @@
                 <form class="floating-labels" method="POST" id="loginform" action="{{ route('login') }}">
                     {{ csrf_field() }}    
                 <h3 class="box-title m-b-20">Sign In</h3>
-                    <div class="text-center form-group{{ $errors->has('error') ? ' has-error' : ''}}">
-                        @if($errors->has('error'))
-                        <p class="help-block">{{ $errors->first('error') }}</p>
-                        @endif
-                    </div>
-                    <div class="form-group m-b-40 m-t-15">
-                        <input type="text" name="username" id="username" class="form-control" style="border-bottom-color:#c0c0c0" required>
-                        <span class="highlight"></span><span class="bar"></span>
-                        <label for="username">Username</label>
-                    </div>
-                    <div class="form-group m-b-10 m-t-10">
-                        <input type="password" name="password" id="password" class="form-control" style="border-bottom-color:#c0c0c0" required>
-                        <span class="highlight"></span><span class="bar"></span>
-                        <label for="password">Password</label>
-                    </div>
+                   
                    <!-- <div class="form-group{{ $errors->has('username') ? ' has-error' : ''}}">
                         <div class="col-xs-12">
                             <input class="form-control" type="text" name="username" required="" placeholder="Username">
@@ -44,21 +30,42 @@
                         <p class="help-block">{{ $errors->first('password') }}</p>
                         @endif
                     </div>-->
-                    
-                    <div class="form-group m-b-10 m-t-15">
-                        <!--<div class="col-md-12">
-                            <div class="checkbox checkbox-primary pull-left p-t-0" style>
-                                <input id="checkbox-signup" type="checkbox">
-                                <label for="checkbox-signup"> Remember me </label>
-                            </div>
-<<<<<<< HEAD
-                            -->
-=======
->>>>>>> resetemail
-                            <a href="{{ route('password.email') }}" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> Forgot pwd?</a> </div>
+                        <div class="form-group{{ $errors->has('error') ? ' has-error' : ''}}">
+                            @if($errors->has('error'))
+                                <b><p class="help-block text-center text-danger">{{ $errors->first('error') }}</p></b>
+                            @endif
+                        </div>
+
+                    <div class="form-group m-b-20 m-t-10{{ $errors->has('username') ? ' has-error' : ''}}">
+                        <input type="text" name="username" id="username" class="form-control" required>
+                        <span class="highlight"></span><span class="bar"></span>
+                        <label for="username">Username</label>
+                        @if($errors->has('username'))
+                        <p class="help-block">{{ $errors->first('username') }}</p>
+                        @endif
+                    </div>
+                 
+                    <div class="form-group m-b-15 m-t-15{{ $errors->has('password') ? ' has-error' : ''}}">
+                        <input type="password" name="password" id="password" class="form-control" required>
+                        <span class="highlight"></span><span class="bar"></span>
+                        <label for="password">Password</label>
+                        @if($errors->has('password'))
+                        <p class="help-block">{{ $errors->first('password') }}</p>
+                        @endif
                     </div>
                     
-                    <div class="form-group text-center m-t-10">
+                    <div class="form-group m-b-15 m-t-15">
+                        <div class="col-md-12">
+                          <!--  <div class="checkbox checkbox-primary pull-left p-t-0" style>
+                                <input id="checkbox-signup" type="checkbox">
+                                <label for="checkbox-signup"> Remember me </label>
+                            </div>-->
+                            
+                            <a href="{{ route('password.email') }}" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> Forgot pwd?</a>
+                             </div>
+                    </div>
+                    
+                    <div class="form-group text-center m-t-40">
                         <div class="col-xs-12">
                             <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" style="background-color:#ee4b28;border:2px solid #ee4b28" type="submit">Log In</button>
                         </div>
@@ -78,15 +85,12 @@
                             <p>Don't have an account? <a href="{{ route('register') }}" class="text-primary m-l-5"><b>Sign Up</b></a></p>
                         </div>
                     </div>
-<<<<<<< HEAD
                 
                 
                 
-=======
                 </form>
                 
                 </form>
->>>>>>> resetemail
             </div>
         </div>
     </section>
