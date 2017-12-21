7bb638620ab85467b784c2f437c08d22b84c78bd
Mitch Alforque <malforque717@gmail.com>
2017-12-21 20:23:03 +0800

    change to floating-label

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,12 +1,74 @@
 @extends('layouts.loginapp')
 @section('content')
 <section id="wrapper" class="login-register">
-        <div class="login-box" style="margin-top:3%">
+        <div class="login-box" style="">
             <div class="white-box">
-                <form class="form-horizontal form-material"  method="POST" id="loginform" action="{{ route('register') }}">
+                <form class="form-horizontal" method="POST" id="loginform" action="{{ route('register') }}">
                     {{ csrf_field() }}
+                    
                     <h3 class="box-title m-b-20">Sign Up</h3>
-                    <div class="form-group{{ $errors->has('name') ? ' has-error' : ''}} ">
+                    <div class="floating-labels">
+                  <div class="form-group{{ $errors->has('name') ? ' has-error' : ''}} ">
+                    <input type="text" name="name" id="name" class="form-control" required>
+                    <span class="highlight"></span><span class="bar"></span>
+                    <label for="name">Name</label>
+                    @if($errors->has('name'))
+                            <p class="help-block">{{ $errors->first('name') }}</p>
+                        @endif
+                  </div>
+                  </div>
+                  <div class="floating-labels">
+                  <div class="form-group{{ $errors->has('email') ? ' has-error' : ''}} ">
+                    <input type="text" name="email" id="email" class="form-control" required>
+                    <span class="highlight"></span><span class="bar"></span>
+                    <label for="email">Email</label>
+                    @if($errors->has('email'))
+                            <p class="help-block">{{ $errors->first('email') }}</p>
+                        @endif
+                  </div>
+                  </div>
+                  <div class="floating-labels">
+                  <div class="form-group{{ $errors->has('username') ? ' has-error' : ''}} ">
+                    <input type="text" name="username" id="username" class="form-control" required>
+                    <span class="highlight"></span><span class="bar"></span>
+                    <label for="username">Username</label>
+                    @if($errors->has('username'))
+                            <p class="help-block">{{ $errors->first('username') }}</p>
+                        @endif
+                  </div>
+                  </div>
+                  <div class="floating-labels">
+                  <div class="form-group{{ $errors->has('password') ? ' has-error' : ''}} ">
+                    <input type="password" name="password" id="password" class="form-control" required>
+                    <span class="highlight"></span><span class="bar"></span>
+                    <label for="password">Password</label>
+                    @if($errors->has('password'))
+                            <p class="help-block">{{ $errors->first('password') }}</p>
+                        @endif
+                  </div>
+                  </div>
+                  <div class="floating-labels">
+                  <div class="form-group ">
+                    <input type="password" name="password_confirmation" id="confirm" class="form-control" required>
+                    <span class="highlight"></span><span class="bar"></span>
+                    <label for="confirm">Confirm Password</label>
+                  </div>
+                  </div>
+                <div class="form-group text-center{{ $errors->has('type') ? ' has-error' : ''}}">
+                          <small class="{{ $errors->has('type') ? ' text-danger' : ''}}">Select Type</small>
+                    <div class="text-center">
+                        <label class="radio-inline radio-primary">
+                            <input type="radio" name="type" id="" value="bidder"><span>Bidder</span>
+                        </label>
+                        <label class="radio-inline radio-primary">
+                            <input type="radio" name="type" id="" value="seeker"><span>Seeker</span>
+                        </label>
+                        @if($errors->has('type'))
+                            <p class="help-block">{{ $errors->first('type' )}}</p>
+                        @endif
+                    </div>
+                </div>
+                   <!-- <div class="form-group{{ $errors->has('name') ? ' has-error' : ''}} ">
                         <div class="col-xs-12">
                             <input class="form-control" type="text" name="name" required="" placeholder="Full Name">
                         </div>
@@ -42,8 +104,9 @@
                         <div class="col-xs-12">
                             <input class="form-control" type="password" name="password_confirmation" required="" placeholder="Confirm Password">
                         </div>
-                    </div>
-                    <div class=" text-center form-group{{ $errors->has('type') ? ' has-error' : ''}} ">
+                    </div>-->
+                  
+                   <!-- <div class=" text-center form-group{{ $errors->has('type') ? ' has-error' : ''}} ">
                         <div class="form-check form-check-inline">
                             <label  class="form-check-label">
                                 <input class="form-check-input" require type="radio" name="type" id="" value="biddeR"> Bidder
@@ -57,7 +120,7 @@
                         @if($errors->has('type'))
                             <p class="help-block">{{ $errors->first('type' )}}</p>
                         @endif
-                    </div>
+                    </div>-->
                     <!--
                     <div class="form-group">
                         <div class="col-md-12">
