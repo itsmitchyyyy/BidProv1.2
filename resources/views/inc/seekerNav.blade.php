66bafad549ea6f489617e2b2dfb0f1ed78862237
mitch alforque <malforque717@gmail.com>
2017-12-28 16:35:16 +0800

    added active class if request is true

--- resources/views/inc/seekerNav.blade.php
+++ resources/views/inc/seekerNav.blade.php
@@ -2,18 +2,18 @@
 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
 </button>
-<a class="navbar-brand" href="#"><img src="/img/bidprologo.png" style="width:100px"></a>
+<a class="navbar-brand" href="{{ route('seeker') }}"><img src="/img/bidprologo.png" style="width:100px"></a>
 <div class="collapse navbar-collapse" id="navbarNavDropdown">
   <ul class="navbar-nav mr-auto">
-    <li class="nav-item active">
-      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    <li class="nav-item{{ Request::is('seeker') ? ' active' : ''}}">
+      <a class="nav-link" href="{{ route('seeker') }}">Home <span class="sr-only">(current)</span></a>
     </li>
-    <li class="nav-item">
+   <!--<li class="nav-item">
       <a class="nav-link" href="#">Features</a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#">Pricing</a>
-    </li>
+    </li>-->
     </ul>
     <ul class="navbar-nav">
     <li class="nav-item dropdown">
