8e1736ca41d697de9fede8ceef45c8d920579cc2
mitch alforque <malforque717@gmail.com>
2017-12-28 18:27:51 +0800

    functions for bidder

--- app/Http/Controllers/BidderController.php
+++ app/Http/Controllers/BidderController.php
@@ -17,6 +17,91 @@ class BidderController extends Controller
    public function bidderView(){
        return view('admin/developers');
    }
+   public function bidderProfile($id){
+       $users = User::findOrFail($id);
 
+       return view('userprofiles/bidder')->with(array('data'=>$users));
+   }
+
+   public function updatePassword(Request $request, $id){
+       $messages = ['current_password.required' => 'Please enter current password'];
    
+    if(Auth::check()){
+        $validator = Validator::make($request->all(),[
+            'current_password' => 'required',
+            'password' => 'required|min:6',
+            'password_confirmation' => 'required|min:6|same:password',
+        ], $messages);
+        if($validator->fails()){
+            return redirect('/bidder/profile',$id)
+            ->withInput(['tab' => 'password'])
+            ->withErrors($validator);
+        }else{
+            $current = Auth::user()->password;
+            if(Hash::check($request->current_password, $current)){
+                $user = User::find($id);
+                $user->password = bcrypt($request->password);
+                $user->save();
+                return redirect('/bidder/profile',$id)
+                    ->withInput(['tab' => 'password'])
+                    ->with('success', 'Password Updated');
+            }else{
+                return redirect('/bidder/profile'.$id)
+                    ->withInput(['tab' => 'password'])
+                    ->withErrors(['current_password' => 'Current password is invalid']);
+            }
+        }
+    }
+    }
+
+    public function updateProfile(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|string|email',
+            'name' => 'required|max:255',
+            'contact' => 'sometimes|nullable|digits:11',
+        ]);
+        if($validator->fails()){
+            return redirect('/bidder/profile/'.$id)
+            ->withInput(['tab' => 'settings'])
+            ->withErrors($validator);
+        }else{
+         $user = User::find($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->contact = $request->contact;
+        $user->address = $request->address;
+        $user->save(); 
+       return redirect('/bidder/profile/'.$id)
+            ->withInput(['tab' => 'settings'])
+            ->with('success','Profile updated');
+        }
+    }
+   
+    public function updateAvatar(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(),[
+            'avatar' => 'image',
+        ]);
+
+        if($validator->fails()){
+            return redirect('/bidder/profile/'.$id)
+                ->withErrors($validator);
+           
+        }else{
+            if($request->hasFile('avatar')){
+                $user = User::find($id);
+                $file = $request->file('avatar');
+                $file->move('uploads', $file->getClientOriginalName());
+                $image_path = "uploads/". $file->getClientOriginalName();
+               // Image::make($file->getRealPath())->resize(225,225)->save($image_path);
+                //$image = Image::make(sprintf('resize/%s', $file->getClientOriginalName()))-resize(225,225)->save();
+                $user->avatar = $image_path;
+                $user->save();
+
+                return redirect('/bidder/profile/'.$id);
+            }
+        }
+        
+    }
 }
