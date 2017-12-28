ef9f56bcf6b91551b6f43bea08d904dd07ec215f
mitch alforque <malforque717@gmail.com>
2017-12-28 17:11:28 +0800

    new ui seeker

--- resources/views/users/seeker.blade.php
+++ resources/views/users/seeker.blade.php
@@ -11,9 +11,59 @@
   POST
 </button>
 <!-- ADD PROJECT -->
+<div class="row">
+  <div class="col-md-3 push-md-9">
+  @foreach($projects as $project)
+<div class="card w-100 m-t-10 m-b-5">
+<div class="card-header">{{ ucwords($project->title) }}
 
-@foreach($projects as $project)
-<div class="card w-75 m-t-10 m-b-5">
+</div>
+<div class="card-block">
+    
+  <h4 class="card-title ml-2">{{ ucwords($project->name) }} </h4>
+    <div class="row">
+        <div class="col-md-12">
+        @if($project->avatar == null)
+            <img src="uploads/blank.png" style="float:left;width:130px;height:100px; margin-right:10%; border-radius:50%;" alt="">
+        @else
+           <img src="{{ $project->avatar }}" style="float:left;width:130px;height:100px; margin-right:10%; border-radius:50%;" alt="">          
+        @endif  
+            <p class="card-text">
+            
+            </p>
+            
+        </div>
+    </div>
+  <button type="button" class="btn btn-info text-uppercase waves-effect waves-light float-right" style="background-color:#ee4b28;border:2px solid #ee4b28;" data-toggle="modal" data-target="#viewBidder{{ $project->project_id }}">View More</button>
+</div>
+</div>
+
+ <!-- START OF VIEW PROFILE -->
+ <div class="modal fade" id="viewProject{{ $project->project_id }}" tabindex="-1" role="dialog" aria-labelledby="viewProfileLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h5 class="modal-title" id="viewProfileLabel">Project Details</h5>
+      </div>
+      <div class="modal-body">
+        Details: {{ $project->details }}
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary wew" data-dismiss="modal" >Close</button>
+        <!--<button type="submit" class="btn btn-primary wew" style="background-color:#ee4b28;border:2px solid #ee4b28">Delete</button>-->
+      </div>
+    </div>
+  </div>
+</div>
+  <!-- END OF VIEW PROFILE -->
+@endforeach
+  </div>
+  <div class="col-md-9 pull-md-3">
+  @foreach($projects as $project)
+<div class="card w-100 m-t-10 m-b-5">
 <div class="card-header">{{ ucwords($project->title) }}
 
 </div>
@@ -59,6 +109,9 @@
 </div>
   <!-- END OF VIEW PROFILE -->
 @endforeach
+  </div>
+</div>
+
 <!-- ADD PROject -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-md" role="document">
