da71dfc119c7fbfc0085bc91e04c27b9b577af8c
itsmitchy <malforque717@gmail.com>
2018-01-03 11:18:05 +0800

    change cost to max and min

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -18,7 +18,8 @@ class ProjectController extends Controller
             'start' => 'required|date',
             'end' => 'required|date',
             'category' => 'required|not_in:0',
-            'cost' => 'required|regex:'.$regex
+            'min' => 'required|regex:'.$regex,
+            'max' => 'required|regex:'.$regex
         ]);
         if($validator->fails()){
             return redirect()->route('seeker')
@@ -33,13 +34,26 @@ class ProjectController extends Controller
         $projects->start = $request->start;
         $projects->end = $request->end;
         $projects->category = $request->category;
-        $projects->cost = $request->cost;
+        $projects->min = $request->min;
+        $projects->max = $request->max;
         $projects->save();
 
         return redirect()->route('seeker')->with('success','Data added');
 
     }
     
+    public function recentProjects()
+    {
+        $projects = DB::table('projects')
+            ->where('user_id', '=', Auth::user()->id)
+            ->orderByRaw('created_at DESC')
+            ->limit(2)
+            ->get();
+
+            return view('users/seeker')->with(array('projects'=>$projects));
+          
+    }
+
     public function getProjects(){
         $projects = DB::table('projects')
             ->join('users', 'projects.user_id', '=', 'users.id')
