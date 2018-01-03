2ffc55ceffd992da3ab9d78a5987f07e7ecf7cd8
itsmitchy <malforque717@gmail.com>
2018-01-03 11:22:37 +0800

    added bids table

---
+++ database/migrations/2017_12_31_072053_create_bids_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBidsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bids', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('seeker_id')->unsigned();
+            $table->integer('bidder_id')->unsigned();
+            $table->foreign('seeker_id')->references('id')->on('users');
+            $table->foreign('bidder_id')->references('id')->on('users');
+            $table->string('duration');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bids');
+    }
+}
