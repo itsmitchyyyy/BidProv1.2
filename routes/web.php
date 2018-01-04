6a2b483b4e98b911660aa1cbbd15d9379bc6212b
itsmitchy <malforque717@gmail.com>
2018-01-04 14:23:06 +0800

    added for route for closed project

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,8 @@ Route::group(['middleware' => ['auth']], function(){
     }]);
     //seeker
     Route::get('seeker/projects', ['as' => 'projects', 'uses' => 'ProjectController@myProjects']);
+    //Route::get('seeker/projects', ['uses' => 'ProjectController@closedProjects']);
+    Route::patch('seeker/projects/update/{id}', ['as' => 'closeproject', 'uses' => 'ProjectController@closeProject']);
     Route::patch('seeker/projects/{id}', ['as' => 'updateproject', 'uses' => 'ProjectController@updateProject']);
     Route::delete('/seeker/projects/delete/{id}', ['as' => 'deleteproject', 'uses' => 'ProjectController@deleteProject']);
     Route::get('seeker', ['as' => 'seeker', 'uses' => 'ProjectController@recentProjects']);
