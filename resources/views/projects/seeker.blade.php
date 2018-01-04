c183ab69faa5416b34c49a1f78efeeb288806e39
itsmitchy <malforque717@gmail.com>
2018-01-04 14:24:13 +0800

    alert update

--- resources/views/projects/seeker.blade.php
+++ resources/views/projects/seeker.blade.php
@@ -19,7 +19,8 @@
   </ul>
 
     @if(session()->has('success'))
-    <div class="alert alert-success">
+    <div class="alert alert-success alert-dismissable fade show">
+      <button type="button" data-dismiss="alert" class="close"><i class="fa fa-close"></i></button>
         {{ session()->get('success') }}
     </div>
     @endif
@@ -165,7 +166,11 @@
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary wew" data-dismiss="modal">Cancel</button>
+          <form action="{{ route('closeproject', ['id' => $project->id]) }}" method="POST">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <input type="hidden" name="_method" value="PATCH">
           <button type="submit" class="btn btn-primary wew" style="background-color:#ee4b28;border:1px solid #ee4b28;">Yes</button>
+          </form>
         </div>
       </div>
     </div>
@@ -178,8 +183,146 @@
 <!-- END OF TABLE -->
 </div>
 <div class="tab-pane" id="closed">
-<h3>asdasd</h3>
+<!-- TABLE -->
+<table id="myProjectClose" class="table table-bordered table-striped" width="100%" cellspacing="0">
+  <thead>
+    <tr>
+      <th>Project Name</th>
+      <th>Number of Bids</th>
+      <th>Highest Bid</th>
+      <th>Bid Duration</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+  <tfoot>
+  <tr>
+      <th>Project Name</th>
+      <th>Number of Bids</th>
+      <th>Highest Bid</th>
+      <th>Bid Duration</th>
+      <th>Action</th>
+    </tr>
+  </tfoot>
+  <tbody>
+  @foreach($closedprojects as $project)
+  <tr>
+    <td><a href="#"><b>{{ ucwords($project->title) }}</b></a></td>
+    <td>{{ $project->title }}</td>
+    <td>{{ $project->title }}</td>
+    <td>{{ $project->title }}</td>
+    <td>
+      <button class="btn btn-link wew " title="Repost" data-tooltip="true" data-toggle="modal" data-target="#repostModal{{ $project->id }}"><i class="fa fa-pencil-square-o"></i></button>
+      <button class="btn btn-link wew " data-tooltip="true" title="Delete" data-toggle="modal" data-target="#closedeleteModal{{ $project->id }}"><i class="text-danger fa fa-trash"></i></button>
+    </td>
+  <!-- EditMODAL -->
+    <div class="modal fade" id="repostModal{{ $project->id }}" tabindex="-1" role="dialog" aria-labelledby="repostModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+           <button type="button" class="close" data-dismiss="modal">
+            <span aria-hidden="true">&times;</span>
+          </button>
+            <h3 class="modal-title" id="repostModalLabel">Repost Project</h3>
+          </div>
+          <div class="modal-body">
+            <form action="" class="form-horizontal" method="POST">
+              <div class="floating-labels">
+                <div class="form-group{{ $errors->has('title') ? ' has-error' : ''}} m-b-40 m-t-15">
+                  <input type="text" name="title" id="title" class="form-control" value="{{ $project->title }}" required>
+                  <span class="highlight"></span><span class="bar"></span>
+                  <label for="titles" class="text-dark">Title</label>
+                  @if($errors->has('title'))
+                    <p>{{ $errors->first('title') }}</p>
+                  @endif
+                </div>
+                <div class="form-group{{ $errors->has('details') ? ' has-error' : ''}} m-b-40 m-t-15">
+                  <textarea name="details" id="details" class="form-control" rows="4" style="height:auto" required>{{ $project->details}}</textarea>
+                  <span class="highlight"></span><span class="bar"></span>
+                  <label for="details" class="text-dark">Details</label>
+                  @if($errors->has('details'))
+                    <p>{{ $errors->first('details') }}</p>
+                  @endif
+                </div>
+                <div class="form-group m-b-40 m-t-15">
+                  <input type="text" name="start" value="{{ $project->start }}" id="start" class="form-control" onfocus="(this.type='date')" onblur="(this.type='text')" required>
+                  <span class="highlight"></span><span class="bar"></span>
+                  <label for="start" class="text-dark">Date Start</label>
+                </div>
+                <div class="form-group m-b-40 m-t-15">
+                  <input type="text" name="end" value="{{ $project->end }}" id="end" class="form-control" onfocus="(this.type='date')" onblur="(this.type='text')" required>
+                  <span class="highlight"></span><span class="bar"></span>
+                  <label for="end" class="text-dark">Date End</label>
+                </div>
+                <div class="form-group{{ $errors->has('category') ? ' has-error' : ''}} m-b-40 m-t-15">
+                  <select name="category" id="category" class="form-control">
+                    <?php $category = array('--' => '--', 'Mobile Development' => 'Mobile', 'Web Development' => 'Web'); ?>
+                    @foreach($category as $categor => $val)
+                      @if($val == $project->category)
+                        <option value="{{ $val }}" selected>{{ $categor }}</option>
+                      @else
+                      <option value="{{ $val }}">{{ $categor }}</option>
+                      @endif
+                    @endforeach
+                 </select>
+                 <span class="highlight"></span><span class="bar"></span>
+                 <label for="category" class="text-dark">Category</label>
+                 @if($errors->has('category'))
+                    <p>{{ $errors->first('category') }}</p>
+                 @endif
+                </div>
+                <div class="form-group m-b-40 m-t-15">
+                  <input type="number" name="min" value="{{ $project->min }}" id="min" class="form-control" step="any" required>
+                  <span class="highlight"></span><span class="bar"></span>
+                  <label for="min" class="text-dark">Min Cost</label>
+                </div>
+                <div class="form-group m-b-40 m-t-15">
+                  <input type="number" name="max" value="{{ $project->max }}" id="max" class="form-control" step="any" required>
+                  <span class="highlight"></span><span class="bar"></span>
+                  <label for="max" class="text-dark">Max Cost</label>
+                </div>
+              </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary wew" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary wew" style="background-color:#ee4b28;border:1px solid #ee4b28;">Repost</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  <!-- END OF REPOST MODAL -->
 
+  <!-- CLOSED DELETE MODAL -->
+  <div class="modal fade" tabindex="-1" role="dialog" id="closedeleteModal{{ $project->id }}">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
+          <h3 class="modal-title">Delete Project</h3>
+        </div>
+        <div class="modal-body">
+          <h3><b>Are you sure you want to delete this project for <i class="fa fa-rouble"></i>200 PHP?</b></h3>
+          <p style="font-weight:bold;margin-top:5%">Deleting your project will permanently remove it from our site so that no one will be able to view it. 
+          All bids and attached files will be erased.
+          </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary wew" data-dismiss="modal">Cancel</button>
+          <form action="{{ route('deleteproject', ['id' => $project->id]) }}" method="POST">
+            {{ csrf_field() }}
+            <input type="hidden" name="_method" value="DELETE">
+            <button type="submit" class="btn btn-primary wew" style="background-color:#ee4b28;border:1px solid #ee4b28">Yes</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--END OF CLOSED DELETE MODAL -->
+  </tr>
+  @endforeach
+  </tbody>
+</table>
+<!-- END OF TABLE -->
 </div>
 </div>
 <!-- ADD PROject -->
