00b82c398959c642c14d589fb8c64586356a9bcc
itsmitchy <malforque717@gmail.com>
2018-01-14 01:42:36 +0800

    paypal

---
+++ config/paypal.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'client_id' => 'AZRjzu0PP1Xki-cq4mnRm7_T2aFWz14mrKKUVqrN12tBoLxgPQKB9B--nVK4Bptj68bBiNKp6UwFJVR6',
+    'secret' => 'EIdSVXIe3PlcEO5fIdiEPz4k7DlvWfTw0nLj2b5UhOrDrIhVZP7h-lQtJgmOhvJaodjCkpOtpcA2d44p',
+    'settings' => array(
+        'mode' => 'sandbox',
+        'http.ConnectionTimeOut' => 1000,
+        'log.LogEnabled' => true,
+        'log.FileName' => storage_path() . '/logs/paypal.log',
+        'log.LogLevel' => 'FINE'
+    ),
+);
