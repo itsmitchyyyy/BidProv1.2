df7f4489e5787796cab1683a83737e70526bb768
itsmitchy <malforque717@gmail.com>
2018-01-22 18:54:35 +0800

    added relationships to proposal

--- app/Project.php
+++ app/Project.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+
+
     public function users(){
         return $this->belongsToMany(User::class);
+    }
+    public function proposals(){
+        return $this->hasMany(Proposal::class, 'project_id');
     }
 }
