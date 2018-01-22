12e07315a7ee271b2e5bb7969551eb20c833bf6f
itsmitchy <malforque717@gmail.com>
2018-01-22 18:52:47 +0800

    added proposal views

--- resources/views/proposal/bidder.blade.php
+++ resources/views/proposal/bidder.blade.php
@@ -1,8 +1,54 @@
 @extends('layouts.bidderapp')
 @section('content')
-<div class="container-fluid">
-<div class="card">
+<div class="container-fluid m-t-15">
 
+@foreach($proposals as $proposal)
+
+<h3><b>{{ ucfirst($proposal->title) }}</b></h3>
+<div class="card">
+    <div class="card-block">
+        <div style="border-right:1px solid black; width:4em;" class="pull-left "><p><b>BIDS</b><br>@if($proposal->proposals->isEmpty()) 0 @else {{ count($proposal->proposals) }} @endif</p></div>
+        <div style="border-right:1px solid black;width:10em;" class="pull-left m-l-10 "><p><b>Avg Bid (PHP)</b><br><span>&#8369;</span>@if($proposal->proposals->isEmpty()) 0 @else {{ number_format($avg,2) }} @endif</p></div>
+        <div style="width:20em" class="pull-left m-l-10"><p><b>Project Budget (PHP)</b><br><span>&#8369;</span> {{ $proposal->min }} - <span>&#8369;</span> {{ $proposal->max }}</p></div>
+        <div class="pull-right m-r-15"><p><b>{{ Carbon\Carbon::parse($proposal->duration)->diffForHumans() }}</b><h3 class="text-center">@if($proposal->duration < Carbon\Carbon::now()) CLOSED @else OPEN @endif</h3></p></div>
+    </div>
 </div>
+<div class="card m-t-15">
+    <div class="card-block">
+        <small><b>Project ID: {{ $proposal->id }} </b></small>
+        <h4 class="card-title"><b>Project description</b></h4>
+        <p class="card-text" style="max-width:40em;word-wrap:break-word">{{ $proposal->details }}</p>
+        <p class="card-text">
+        <h4><b>About the seeker</b></h4>
+        <i class="fa fa-star" style="color:orange"></i>
+        <i class="fa fa-star" style="color:orange"></i>
+        <i class="fa fa-star" style="color:orange"></i>
+        <i class="fa fa-star-half-full" style="color:orange"></i>
+        <i class="fa fa-star-o" style="color:orange"></i>
+        </p>
+        <div class="pull-right">
+        @if($proposal->duration < Carbon\Carbon::now())
+        <button disabled class="btn btn-info wew">Bid</button>
+        @else
+        <button  class="btn btn-info wew">Bid</button>
+        @endif
+        </div>
+    </div>
+</div>
+@endforeach
+<table class="table table-bordered m-t-15">
+<tr class="bg-success">
+<th class="text-white w-50">BIDDING(50)</th>
+<th class="text-white">REPUTATION</th>
+<th class="text-white">BID (PHP)</th>
+</tr>
+<?php for($i = 1; $i < 51; $i++){?>
+<tr>
+<td><?php echo $i; ?></td>
+<td>test</td>
+<td>test</td>
+</tr>
+<?php } ?>
+</table>
 </div>
 @endsection
