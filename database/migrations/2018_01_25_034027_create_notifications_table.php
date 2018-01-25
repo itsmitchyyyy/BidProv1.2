462056dfd7b78b2a42807552adba824a2fac69eb
itsmitchy <malforque717@gmail.com>
2018-01-25 18:39:04 +0800

    updated notification table

--- database/migrations/2018_01_25_034027_create_notifications_table.php
+++ database/migrations/2018_01_25_034027_create_notifications_table.php
@@ -15,7 +15,7 @@ class CreateNotificationsTable extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('name');
             $table->string('message');
