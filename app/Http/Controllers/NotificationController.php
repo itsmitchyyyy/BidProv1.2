2900b1d92051c7d995ec1c7e8055c55fa5822619
itsmitchy <malforque717@gmail.com>
2018-01-25 16:03:26 +0800

    added notification controller

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Notification;
+use DB;
+use Illuminate\Support\Facades\Auth;
+class NotificationController extends Controller
+{
+    //
+    public function viewNotification(){
+        $notifications = Notification::where('user_id', Auth::user()->id)->orderBy('created_at', 'desc')->get();
+        return view('notifications/seeker')->with(compact('notifications'));
+    }
+    public function navNotification(){
+        $notifications = Notification::where('user_id', Auth::user()->id)->orderBy('created_at', 'desc')->get();
+        return $notifications;
+    }
+}
