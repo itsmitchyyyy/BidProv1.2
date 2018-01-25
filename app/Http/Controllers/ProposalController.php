a7b7c8ea3b5e0fd1429e9fe6bb5ea5fa832045ba
itsmitchy <malforque717@gmail.com>
2018-01-25 16:03:48 +0800

    added proposal controller

---
+++ app/Http/Controllers/ProposalController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Proposal;
+use App\Project;
+use DB;
+use Illuminate\Support\Facades\Auth;
+class ProposalController extends Controller
+{
+    public function viewProposals(){
+        $proposal = Proposal::where('bidder_id',Auth::user()->id)->with('projects')->get();
+       /*  $proposal = DB::table('proposals')
+            ->join('projects', 'projects.id', '=', 'proposals.project_id')
+            ->where('proposals.bidder_id', '=', Auth::user()->id)
+            ->get(); */
+        return view('bids/bidder')->with(compact('proposal'));
+    }
+
+    public function cancelProposal(Request $request){
+       $db =  DB::table('project_modules')
+            ->where('proposal_id', $request->pr_id)
+            ->delete();
+         Proposal::where('id', $request->pr_id)->delete();
+        return redirect()->route('bids'); 
+    }
+}
