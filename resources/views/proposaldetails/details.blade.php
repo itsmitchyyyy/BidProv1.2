bb7c8b0d0d0a232f179cd454ec638c1934a62033
itsmitchy <malforque717@gmail.com>
2018-02-01 14:16:29 +0800

    now have ui and datas

--- resources/views/proposaldetails/details.blade.php
+++ resources/views/proposaldetails/details.blade.php
@@ -13,57 +13,134 @@
     max-width:40em;
     word-wrap:break-word;
 }
+.wrap-skill{
+    max-width:10em;
+    word-wrap:break-word;
+}
 </style>
 @endpush
 @section('content')
 <div class="container-fluid m-t-15">
+@inject('controller', 'App\Http\Controllers\ProjectController')
+<h3><b>{{ ucfirst($projects->title) }}</b></h3>
     <div class="card">
         <div class="card-block">
             <div class="row">
                 <div class="col-md-1 col-xs-6 b-r"><strong>BIDS</strong>
-                <br><p class="text-muted">Test</p>
+                <br><p class="text-muted">@if($projects == null) 0 @else {{ count($projects->proposals) }} @endif</p>
                 </div>
-                <div class="col-md-1 col-xs-6 b-r"><strong>Avg Bid (PHP)</strong>
-                <br><p class="text-muted">Test</p>
+                <div class="col-md-2 col-xs-6 b-r"><strong>Avg Bid (PHP)</strong>
+                <br><p class="text-muted"><span>&#8369;</span>@if($projects == null) 0 @else {{ number_format($avg,2) }} @endif</p>
                 </div>
-                <div class="col-md-7 col-xs-6"><strong>Project Budget (PHP)</strong>
-                <br><p class="text-muted">Test</p>
+                <div class="col-md-6 col-xs-6"><strong>Project Budget (PHP)</strong>
+                <br><p class="text-muted"><span>&#8369;</span> {{ $projects->min }} - <span>&#8369;</span> {{ $projects->max }} </p>
                 </div>
-                <div class="col-md-2 col-xs-6"><strong class="pull-right">Days left</strong>
-                <br><p class="text-muted m-r-15 pull-right">Test</p>
+                <div class="col-md-2 col-xs-6"><strong class="pull-right">{{ Carbon\Carbon::parse($projects->duration)->diffForHumans() }}</strong>
+                <br><h3><p class="font-bold text-muted m-r-40 pull-right">@if(Carbon\Carbon::parse($projects->duration) > Carbon\Carbon::now()) OPEN @else CLOSED @endif </p></h3>
                 </div>
             </div>
         </div>
     </div>
-    
-    <div class="card m-t-15">
+    <div class="card m-t-15 mb-5">
         <div class="card-block">
-            <small class="float-right">Just now</small>
+            <small class="float-right">{{ Carbon\Carbon::parse($modules[0]->created_at)->diffForHumans() }}</small>
             <h3 class="card-title">Proposal</h3>
             <hr>
-            <small><b>Proposal ID: 1</b></small>
+            <small><b>Proposal ID: {{ $modules[0]->proposal_id }}</b></small>
             <h4 class="card-title"><b>Proposal Details</b></h4>
             <p class="card-text wrap-content">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit possimus eos, explicabo doloribus unde hic beatae deserunt numquam nam repellat. Atque est nam adipisci obcaecati consequatur tempore consectetur qui minima.
+            @foreach($modules as $key => $module)
+                Module {{ $key + 1}}<br>Description:  {{ ucfirst($module->description) }} <br> Days to do: {{ $module->daysTodo }}<br>
+            @endforeach
+            <?php $max = $controller->getProjectModules($modules[0]->proposal_id); ?>
+            @if($max == 1)
+            Maximum of {{ $max }} day<br>
+            @else
+            Maximum of {{ $max }} days<br
+            @endif
+            Price: <span>&#8369;</span>{{ $modules[0]->price }}
             </p>
             <p class="card-text">
             <h4><b>Bidders Profile</b></h4>
             <div class="clearfix">
-            <img src="/uploads/blank.png" alt="avatar" class="img-thumbnail m-b-15 pull-left gap-right" style="width:100px;height:100px">
+            <img src="/{{ $user->avatar }}" alt="avatar" class="img-thumbnail m-b-15 pull-left gap-right" style="width:100px;height:100px">
             <p class="text-muted">
-            <a href="">Test</a>
-            <br><small>Test</small>
+            <a href="">{{ ucfirst($user->firstname) }} {{ ucfirst($user->lastname) }}</a>
+            <br><small>{{ $user->email }}</small><br>
+            <small>Skills</small>
+            <?php $data = array_slice($skill, 0 ,2) ?>
+            @foreach($data as $key =>  $skills)
+                @if($key == 0 )
+                   <small>{{ $skills }},</small>
+                @else
+                <small>{{ $skills }}</small>
+                @endif
+            @endforeach
             </p>
             </div>
-            <input id="input-1" class="rating rating-loading" data-min="0" data-max="5" data-step="0.1" value="2" data-size="s" disabled>
+            <input id="input-1" class="rating rating-loading" data-min="0" data-max="5" data-step="0.1" value="{{ $user->userAverageRating }}" data-size="s" disabled>
             </p>
+            <div class="pull-right">
+                <a href="#"><button class="btn btn-info wew">Award</button></a>
+            </div>
         </div>
     </div>
+    <table class="table table-bordered m-t-15" id="myBidding">
+        <thead>
+        <tr class="bg-success">
+            <th class="text-white" style="width:50%">BIDDING</th>
+            <th class="text-white">REPUTATION</th>
+            <th class="text-white">BID (PHP)</th>
+            <th class="text-white">ACTION</th>
+        </tr>    
+        </thead>
+        <tbody>
+        @foreach($biddings as $bidding)
+            <tr>
+                <td>
+                    <div class="clearfix">
+                        <img src="/{{ $bidding->avatar }}" alt="avatar" style="height:150px;width:150px" class="pull-left gap-right">
+                        <p class="text-muted">
+                        <a href="">{{ $bidding->firstname }} {{ $bidding->lastname }}</a>
+                        <br><small>{{ Carbon\Carbon::parse($bidding->proposal_created_at)->diffForHumans() }}</small>
+                        </p>
+                    </div>
+                </td>
+                <td>
+                    <input  name="input-1" id="input-1" class="rating rating-loading" data-min="0" data-max="5" data-step="0.1" value="{{ $controller->getBidder($bidding->bidder_id)->userAverageRating }}" data-size="s" disabled>
+                    <p class="text-muted">
+                        User Reviews {{ $controller->getBidder($bidding->bidder_id)->userSumRating }} Reviews
+                    </p>
+                </td>
+                <td>
+                    <span>&#8369;</span> {{ $bidding->price }}
+                    <p class="text-muted">
+                    <?php $max = $controller->getProjectModules($bidding->proposal_id); ?>
+                    @if($max == 1)
+                    in just {{ $max }} day
+                    @else
+                    in a span of {{ $max }} days
+                    @endif
+                    </p>
+                </td>
+                <td>
+<a href="{{ route('viewBids', ['project_id' => $bidding->project_id, 'user_id' => $bidding->bidder_id, 'proposal_id' => $bidding->proposal_id]) }}" data-toggle="tooltip" title="Award" class="gap-right"><i class="fa fa-trophy" style="font-size:24px"></i></a>
+</td>
+
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 </div>
 @endsection
 @section('scripts')
 <script src="{{ asset('js/star-rating.js') }}"></script>
 <script>
     $('#input-id').rating();
+</script>
+<script>
+    $(function(){
+        $('#myBidding').DataTable();
+    });
 </script>
 @endsection
