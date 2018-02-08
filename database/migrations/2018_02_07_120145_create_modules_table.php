dfe0711ec4471137ed9f019a30dedd88db258113
itsmitchy <malforque717@gmail.com>
2018-02-08 16:20:43 +0800

    added migration module table

---
+++ database/migrations/2018_02_07_120145_create_modules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('proposal_id')->unsigned();
+            $table->foreign('proposal_id')->references('id')->on('proposals');
+            $table->string('module_name');
+            // $table->decimal('price', 8,2);
+            // $table->string('daysTodo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modules');
+    }
+}
