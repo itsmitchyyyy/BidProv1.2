0d604f1061e25864f6d4f661fd47ec97bec44426
itsmitchy <malforque717@gmail.com>
2018-02-08 16:20:22 +0800

    added migration proposal module

---
+++ database/migrations/2018_02_07_120710_create_proposal_modules_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProposalModulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proposal_modules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('module_id')->unsigned();
+            $table->foreign('module_id')->references('id')->on('modules');
+            $table->string('description');
+            $table->string('percentDone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proposal_modules');
+    }
+}
