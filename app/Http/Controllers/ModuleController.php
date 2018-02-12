9302187bcbc6dbccee4f45c421c61fbb0d01cb0c
itsmitchy <malforque717@gmail.com>
2018-02-12 17:08:39 +0800

    added comment temporary

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -8,7 +8,11 @@ use App\Bid;
 use App\Project;
 use App\User;
 use App\Proposal;
+use Session;
 use DB;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 class ModuleController extends Controller
 {
     public function getModule($proposal_id,$seeker_id,$project_id)
@@ -48,10 +52,44 @@ class ModuleController extends Controller
             ->with(compact('todo','doing','done','project','proposal'));
     }
     public function proposalModules(){
+        session()->forget('module_id');
+        session()->regenerate();
         $module_id = $_GET['module_id'];
         $modules = DB::table('proposal_modules')
             ->where('proposal_modules.module_id',$module_id)
             ->get();
+        session()->put('module_id', $module_id);
         return json_encode($modules);
     }
+
+    public function addComment(Request $request){
+        $message = ['required' => 'The comment field must not be empty'];
+        $validator = Validator::make($request->all(),[
+            'progress_comment' => 'required|max:255',
+        ], $message);
+        if($validator->fails()){
+            return redirect()
+                ->route('acceptedBid',[$request->comment_proposal,Auth::user()->id,$request->comment_project])
+                ->withErrors($validator);
+        }
+        $ok = DB::table('module_comments')
+            ->insert([
+                'user_id' => Auth::user()->id,
+                'module_id' => $request->comment_module,
+                'message' => $request->progress_comment,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]); 
+             return redirect()
+            ->route('acceptedBid',[$request->comment_proposal,Auth::user()->id,$request->comment_project])
+            ->with('success','Comment added');
+     }
+
+     public function moduleComments($id){
+         $comments = DB::table('module_comments')
+            ->where('module_id',$id)
+            ->get();    
+            // dd($comments);
+        return $comments;
+     }
 }
