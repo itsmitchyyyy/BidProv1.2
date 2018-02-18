60630544192d0ca05283adc9960414255030db8d
mitch alforque <malforque717@gmail.com>
2018-02-18 20:11:38 +0800

    added zipper

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         willvincent\Rateable\RateableServiceProvider::class,
         Propaganistas\LaravelPhone\PhoneServiceProvider::class,
+        'Chumper\Zipper\ZipperServiceProvider',
         /*
          * Package Service Providers...
          */
@@ -226,7 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Zipper' => 'Chumper\Zipper\Zipper'
     ],
 
 ];
