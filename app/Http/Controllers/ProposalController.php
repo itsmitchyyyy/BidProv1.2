779ce04aee4b22ba89dc2bf28dfa236265ad4d82
mitch alforque <malforque717@gmail.com>
2018-02-19 06:23:04 +0800

    modified proposal table

--- app/Http/Controllers/ProposalController.php
+++ app/Http/Controllers/ProposalController.php
@@ -4,21 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Proposal;
+use App\Module;
 use App\Project;
+use App\Bid;
 use DB;
 use Illuminate\Support\Facades\Auth;
 class ProposalController extends Controller
 {
     public function viewProposals(){
-        $proposal = Proposal::where('bidder_id',Auth::user()->id)->with('projects')->get();
+        // $proposal = Proposal::where('bidder_id',Auth::user()->id)->with('projects')->get();
+        $proposal = DB::table('proposals')
+            ->where('proposals.bidder_id',Auth::user()->id)
+            ->join('projects','proposals.project_id','=','projects.id')
+            ->join('modules','modules.proposal_id','=','proposals.id')
+            ->where('proposals.status',1)
+            ->select('*','proposals.id as proposal_id','modules.id as module_id')
+            ->get();
         return view('bids/bidder')->with(compact('proposal'));
     }
 
     public function cancelProposal(Request $request){
-       $db =  DB::table('project_modules')
-            ->where('proposal_id', $request->pr_id)
+        DB::table('proposal_modules')
+            ->where('module_id',$request->mod_id)
             ->delete();
-         Proposal::where('id', $request->pr_id)->delete();
-        return redirect()->route('bids'); 
+        DB::table('module_comments')
+            ->where('module_id', $request->mod_id)
+            ->delete();
+        Module::find($request->mod_id)->delete();
+        Proposal::where('id', $request->pr_id)->delete();
+        return redirect()->route('bids')->with('success','Bid Cancelled'); 
+    }
+    public function getModule($proposal_id){
+        $module = Module::where('proposal_id', $proposal_id)->pluck('id')->toArray();
+        return $module;
     }
 }
