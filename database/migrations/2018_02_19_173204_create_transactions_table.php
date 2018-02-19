59c440b5e04179e005457de6a2a381f58869175c
itsmitchy <malforque717@gmail.com>
2018-02-20 06:05:23 +0800

    added transactions table

---
+++ database/migrations/2018_02_19_173204_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('seeker_id')->unsigned();
+            $table->foreign('seeker_id')->references('id')->on('users');
+            $table->integer('bidder_id')->unsigned();
+            $table->foreign('bidder_id')->references('id')->on('users');
+            $table->integer('project_id')->unsigned();
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->integer('payment_id')->nullabe();
+            $table->decimal('amount', 8,2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
