c2ed09cabd030da231617854791e267b31383634
itsmitchy <malforque717@gmail.com>
2018-02-19 19:00:40 +0800

    added reports table for admin

---
+++ database/migrations/2018_02_19_082104_create_reports_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('seeker_id')->unsigned();
+            $table->foreign('seeker_id')->references('id')->on('users');
+            $table->integer('bidder_id')->unsigned();
+            $table->foreign('bidder_id')->references('id')->on('users');
+            $table->string('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
