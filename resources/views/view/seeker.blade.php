4450f3981926daac28f92ae2c84d4e2f798ba4f3
itsmitchy <malforque717@gmail.com>
2018-02-20 06:02:01 +0800

    add view users

--- resources/views/view/seeker.blade.php
+++ resources/views/view/seeker.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.bidderapp')
+@extends('layouts.seekerapp')
 @push('css')
 <style>
 .glyphicon-star:before {
@@ -9,81 +9,166 @@
     content: "\f005";  /* this is your text. You can also use UTF-8 character codes as I do here */
     font-family: FontAwesome;
 }
+.gap-right{
+    margin-right: 10px;
+}
 </style>
 @endpush
 @section('content')
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <h3>REVIEW USER</h3>
-                <div class="panel-body">
-                    <form action="{{ route('rate.postseeker') }}" method="POST">
-                        {{ csrf_field() }}
-                    <div class="card">
-                        <div class="container-fliud">
-                            <div class="wrapper row">
-                                <div class="preview col-md-6">
-                                    <div class="preview-pic tab-content">
-                                      <div class="tab-pane active" id="pic-1">
-                                      @if($user->avatar == null)
-                                      <img src="uploads/blank.png" style="width:500px;height:500px"/>
-                                      @else
-                                      <img src="/{{ $user->avatar }}" style="width:500px;height:500px"/>
-                                      @endif
-                                      </div>
-                                    </div>
-                                </div>
-                                <div class="details col-md-6">
-                                    <h3 class="mt-5">Name: {{ $user->firstname }} {{ $user->lastname }}</h3>
-                                    <h3>Email: {{ $user->email }}</h3>
-                                    <h3>Contact: @if($user->contact == null) NONE @else {{ $user->contact }}@endif</h3>
-                                    <h3>Address: @if($user->address == null) NONE @else {{ $user->address }} @endif</h3>
-                                    <h3 class="product-title mt-5">USER REVIEW</h3>
-                                    <div class="rating">
-                                        <input id="input-1" name="rate" class="rating rating-loading" data-min="0" data-max="5" data-step="1" value="{{ $user->userAverageRating }}" data-size="xs">
-                                        <input type="hidden" name="id" required="" value="{{ $user->id }}">
-                                        <span class="review-no">Average Rating: {{ number_format($user->averageRating,0) }}</span>
-                                        <br/>
-                                        <br>
-                                        <textarea name="comment_review" style="height:auto;border:1px solid rgba(0,0,0,.25)" id="comment_review" class="form-control"  rows="4" placeholder="Write a review here"></textarea><br>
-                                        <button class="btn btn-success wew">Submit Review</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+<div class="modal fade" id="reportModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button class="close" data-dismiss="modal"><span>&times;</span></button>
+                <h3>Report User</h3>
+            </div>
+            <div class="modal-body">
+                <form action="">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                    <label for="message_report">Write your report here</label>
+                        <textarea name="message_report" id="message_report" style="height:auto" class="form-control" rows="5"></textarea>
                     </div>
-                    </form>
-
-                </div>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary wew" data-dismiss="modal">Close</button>
+                <button class="btn btn-info wew">Submit</button>
+                </form>
             </div>
         </div>
     </div>
 </div>
-<div class="container-fluid m-t-15">
-                    <div class="col-md-12 col-lg-12 col-sm-12" >
-                        <div class="white-box " style="border:1px solid rgba(0,0,0,.125); border-radius:.25rem">
-                            <h3 class="box-title">Review Feed</h3>
-                            <hr style="background-color:rgba(0,0,0,.125)">
-                            <div class="comment-center">
-                                @foreach($reviews as $review)
-                                <div class="comment-body" style="border-bottom:1px solid rgba(0,0,0,0.125);">
-                                    <div class="user-img"> <img src="/{{ $review->avatar }}" alt="user" class="img-circle"></div>
-                                    <div class="mail-contnet">
-                                        <h5>{{ ucfirst($review->firstname) }} {{ ucfirst($review->lastname) }}</h5> <span class="mail-desc">{{ $review->comments }}</span><span class="time pull-right">{{ Carbon\Carbon::parse($review->created_at)->diffForHumans() }}</span>
+  <!-- Page Content -->
+            <div id="">
+            <div class="container-fluid">
+                <div class="row bg-title">
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                        <h4 class="page-title">Seeker Profile</h4> </div>
+                </div>
+                <!-- /.row -->
+                <!-- .row -->
+                <div class="row">
+                    <div class="col-md-4 col-xs-12">
+                    <a href="#" data-toggle="modal" data-target="#reportModal" style="color:red"><strong>Report User</strong></a>
+                        <div class="white-box">
+                            <div class="user-bg"> 
+                            <img width="100%" alt="user" src="/uploads/blank.png">
+                                <div class="overlay-box">
+                                    <div class="user-content">
+                                        @if($user->avatar == null)
+                                        <a href="javascript:void(0)" id="newDP" data-tooltip="true" title="Profile"><img src="/uploads/blank.png" id="imageSrc" class="thumb-lg img-circle" alt="img"></a>
+                                        @else
+                                        <a href="javascript:void(0)" id="newDP" data-tooltip="true" title="Profile"><img src="/{{ $user->avatar }}" id="imageSrc" class="thumb-lg img-circle" alt="img"></a>
+                                        @endif
+                                        <h4 class="text-white">{{ ucfirst($user->firstname) }} {{ ucfirst($user->lastname) }}</h4>
+                                        <h5 class="text-white">{{ $user->email }}</h5> 
+                                        <input id="input-1" EMAIl="input-1" class="rating rating-loading" data-min="0" data-max="5" data-step="0.1" value="{{ $user->userAverageRating }}" data-size="s" disabled="">
+                                        </div>
+                                </div>
+                            </div>
+                           <div class="user-btm-box">
+                               
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-8 col-xs-12">
+                        <div class="white-box">
+                            <ul class="nav customtab nav-tabs" id="tabMenu" role="tablist">
+                                <li role="presentation" class="nav-item"><a href="#profile" class="nav-link active" aria-controls="profile" role="tab" data-toggle="tab" aria-expanded="false"><span class="visible-xs"><i class="fa fa-user"></i></span> <span class="hidden-xs">Profile</span></a></li>
+                            </ul>
+                            <div class="tab-content">
+                               
+                                <div class="tab-pane active" id="profile">
+                                    <div class="row">
+                                        <div class="col-md-3 col-xs-6 b-r"> <strong>Full Name</strong>
+                                            <br>
+                                            <p class="text-muted">{{ $user->firstname }} {{ $user->lastname }}</p>
+                                        </div>
+                                        <div class="col-md-3 col-xs-6 b-r"> <strong>Mobile</strong>
+                                            <br>
+                                            <p class="text-muted">{{ $user->mobile_no }}</p>
+                                        </div>
+                                        <div class="col-md-3 col-xs-6 b-r"> <strong>Landline</strong>
+                                            <br>
+                                            <p class="text-muted">{{ $user->landline }}</p>
+                                        </div>
+                                        <div class="col-md-3 col-xs-6"> <strong>Email</strong>
+                                            <br>
+                                            <p class="text-muted">{{ $user->email }}</p>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-3 col-xs-6 b-r"><strong>Street No</strong>
+                                           <br>
+                                            <p class="text-muted">{{ $user->street_no }}</p>
+                                        </div>
+                                        <div class="col-md-3 col-xs-6 b-r"><strong>City</strong>
+                                            <br>
+                                            <p class="text-muted">{{ $user->city }}</p>
+                                        </div>
+                                        <div class="col-md-3 col-xs-6 b-r"><strong>Province</strong>
+                                            <br>
+                                            <p class="text-muted">{{ $user->province }}</p>
+                                        </div>
+                                        <div class="col-md-3 col-xs-6"><strong>Zip Code</strong>
+                                            <br>
+                                            <p class="text-muted">{{ $user->zip_code }}</p>
+                                        </div>
                                     </div>
                                 </div>
-                                @endforeach
                             </div>
                         </div>
                     </div>
                 </div>
-
+            </div>
+            <h3>Projects Done</h3>
+            <table id="projectsDone">
+                <thead>
+                    <tr>
+                        <th>Project Name</th>
+                        <th>Client</th>
+                        <th>Bid</th>
+                        <th>Status</th>
+                    </tr>
+                </thead> 
+                <tbody>
+                    @foreach($projects as $project)
+                    <tr>
+                        <td><a href="#">{{ ucfirst($project->title) }}</a></td>
+                        <td>
+                            <div class="clearfix">
+                                <img src="/{{ $project->avatar }}" alt="avatar" style="width:100px;height:100px" class="gap-right img-thumbnail pull-left">
+                                <p>
+                                    <a href="">{{ ucfirst($project->firstname) }} {{ ucfirst($project->lastname) }}</a>
+                                    <br>
+                                    <input id="input-1" name="input-1" data-min="0" data-step="0.1" data-max="5" value="{{ $user->userAverageRating }}" data-size="s" class="rating rating-loading" disabled>
+                                    Reviews: {{ $user->userSumRating }} Reviews
+                                </p>
+                            </div>
+                        </td>
+                        <td>
+                            <span>&#8369;</span> {{ $project->price }} in {{ $project->daysTodo }} days
+                        </td>
+                        <td>
+                            Done
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <!-- /.container-fluid -->
+            <footer class="footer text-center"> 2017 &copy;  brought to you by BidPro </footer>
+        </div>
+        <!-- /#page-wrapper -->
 @endsection
 @section('scripts')
-<script src="{{ asset('js/landing-page/jquery/jquery.min.js') }}"></script>
 <script src="{{ asset('js/star-rating.js') }}"></script>
-<script type="text/javascript">
-    $("#input-id").rating();
+<script>
+    $('#input-id').rating();
+</script>
+<script>
+    $(function(){
+        $('#projectsDone').DataTable();
+    });
 </script>
 @endsection
