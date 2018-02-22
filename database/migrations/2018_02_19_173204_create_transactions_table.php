cc677e56054e24704065834cc5414be552262d5e
itsmitchy <malforque717@gmail.com>
2018-02-23 06:48:33 +0800

    updates on transaction table

--- database/migrations/2018_02_19_173204_create_transactions_table.php
+++ database/migrations/2018_02_19_173204_create_transactions_table.php
@@ -21,10 +21,10 @@ class CreateTransactionsTable extends Migration
             $table->foreign('bidder_id')->references('id')->on('users');
             $table->integer('project_id')->unsigned();
             $table->foreign('project_id')->references('id')->on('projects');
-            $table->integer('payment_id')->nullabe();
-            $table->decimal('amount', 8,2)->nullable();
-            $table->decimal('commission', 8,2)->nullable();
-            $table->decimal('total', 8,2)->nullable();
+            $table->string('payment_id')->nullabe();
+            $table->decimal('amount', 11,2)->nullable();
+            $table->decimal('commission',11,2)->nullable();
+            $table->decimal('total', 11,2)->nullable();
             $table->string('status')->nullable();
             $table->timestamps();
         });
