c1d92b080b95aa8d06d632b6ee36e728abbb6e1a
itsmitchy <malforque717@gmail.com>
2018-02-23 06:53:52 +0800

    implements shouldbroadcast

--- app/Events/ReportEvent.php
+++ app/Events/ReportEvent.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class ReportEvent
+class ReportEvent implements ShouldBroadCast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
