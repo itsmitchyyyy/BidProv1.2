1a6d8b169a4ce031b810b2d175e41064e210e6b2
itsmitchy <malforque717@gmail.com>
2018-02-22 17:13:12 +0800

    added report controller

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,10 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Report;
+use DB;
+use App\User;
 class ReportController extends Controller
 {
-    public function postReport(){
-
+    public function postReport(Request $request){
+        $report = new Report();
+        $report->seeker_id  = $request->seeker_id;
+        $report->bidder_id = $request->bidder_id;
+        $report->message = $request->message;
+        $report->save();
+        $report_id = $report->id;
+        event(new \App\Events\ReportEvent($request->seeker_id,$request->bidder_id,$request->message,$report_id));
+    }
+    public function getReport(Request $request){
+        $seeker = User::find($request->seeker_id)->all();
+        $bidder = User::find($request->bidder_id)->all();
+        $reports = array(
+            'first' => $seeker,
+            'second' => $bidder  
+        );
+        echo json_encode($reports);
     }
 }
