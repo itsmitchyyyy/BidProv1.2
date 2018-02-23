8b216003c700900f160e908c51ddf476b70030bf
mitch alforque <malforque717@gmail.com>
2018-02-24 06:35:55 +0800

    updates on reportcontroller new functions

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -8,14 +8,17 @@ use DB;
 use App\User;
 class ReportController extends Controller
 {
-    public function postReport(Request $request ){
+    public function postReport(){
+        $seeker_id =  $_POST['seeker_id'];
+        $bidder_id =  $_POST['bidder_id'];
+        $message =  $_POST['message'];
         $report = new Report();
-        $report->seeker_id  = $request->seeker_id;
-        $report->bidder_id = $request->bidder_id;
-        $report->message = $request->message;
+        $report->seeker_id  = $seeker_id;
+        $report->bidder_id = $bidder_id;
+        $report->message = $message;
         $report->save();
         $report_id = $report->id;
-        event(new \App\Events\ReportEvent($request->seeker_id,$request->bidder_id,$request->message,$report_id));
+        event(new \App\Events\ReportEvent($seeker_id,$bidder_id,$message,$report_id));
     }
     public function getReport(){
         $seeker_id = $_POST['seeker_id'];
@@ -27,5 +30,18 @@ class ReportController extends Controller
             'second' => $bidder  
         );
         echo json_encode($reports);
+    }
+
+    public function getReports(){
+        $reports = Report::all();
+        echo json_encode($reports);
+    }
+    
+    public function updateReport(){
+        $report_id = $_POST['report_id'];
+        DB::table('reports')
+            ->where('id',$report_id)
+            ->update(['status' => 'confirm']);
+        echo 'OK';
     }
 }
