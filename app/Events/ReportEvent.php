1741041324ea5c765ac62de54d5fde5827be58ab
mitch alforque <malforque717@gmail.com>
2018-02-24 06:35:27 +0800

    updates

--- app/Events/ReportEvent.php
+++ app/Events/ReportEvent.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class ReportEvent implements ShouldBroadCast
+class ReportEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
