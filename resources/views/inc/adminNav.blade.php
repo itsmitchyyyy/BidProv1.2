161bdd8b085018713b43e28965e4289efac284ae
mitch alforque <malforque717@gmail.com>
2018-02-26 05:25:06 +0800

    added routes for index

--- resources/views/inc/adminNav.blade.php
+++ resources/views/inc/adminNav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-default navbar-static-top m-b-0" >
 <div class="navbar-header" style="background-color:#ccc"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg" href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="ti-menu"></i></a>
-    <div class="top-left-part" style="background-color:#C0C0C0"><a class="logo" href="index.html" style="padding-left:20%;"><b><img src="/img/bidprologo.png" alt="home" style="width:120px;"  /></span></a></div>
+    <div class="top-left-part" style="background-color:#C0C0C0"><a class="logo" href="{{ route('admin') }}" style="padding-left:20%;"><b><img src="/img/bidprologo.png" alt="home" style="width:120px;"  /></span></a></div>
     <ul class="nav navbar-top-links navbar-left hidden-xs">
         
        <!-- <li>
